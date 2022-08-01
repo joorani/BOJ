ba067203cb5fa09178d32a78bc279888ef4414e0
joorani <80900915+joorani@users.noreply.github.com>
2022-08-01 14:10:42 +0900

    [Gold IV] Title: 뱀, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -0,0 +1,81 @@
+'''
+뱀
+'''
+# 입력받기
+n = int(input()) #보드의 크기
+k = int(input()) #사과의 개수
+
+# n *n 크기의 보드판 만들기
+board = [[0] * (n+1) for _ in range(n+1)]
+
+# 사과 입력받기(사과 있는 곳은 1로 표시)
+for i in range(k):
+    a, b = map(int, input().split())
+    board[a][b] = 1
+
+# 뱀의 방향 변환 횟수
+L = int(input())
+
+# 뱀의 방향 저장 리스트
+change_direction = []
+for i in range(L):
+    x, c = input().split()
+    change_direction.append((int(x), c))
+
+# 처음에는 오른쪽을 보고 있으므로 동,남,서,북 방향
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+# 방향 회전
+def turn(direction, c):
+    # 왼쪽으로 방향 전환
+    if c == 'L':
+        direction = (direction - 1) % 4
+    # 오른쪽으로 회전
+    else:
+        direction = (direction + 1) % 4
+    return direction
+
+def simulate():
+    x, y = 1, 1 # 뱀의 머리 위치
+    board[x][y] = 2 # 뱀이 존재하는 위치는 2로 표시
+    direction = 0 #  처음에는 동쪽을 보고 있음
+    time = 0 # 시작한 뒤에 지난 '초' 기록
+    index = 0 # 다음에 회전할 정보
+    q = [(x, y)] #뱀이 차지하고 있는 위치 정보(꼬리가 앞쪽)
+    while True:
+        # 동쪽으로 이동
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+
+        # 맵 범위 안에 있고, 뱀의 몸통이 없는 위치라면
+        if nx >= 1 and ny >= 1 and nx <= n and ny <= n and board[nx][ny] != 2:
+            # 사과가 없다면 이동 후에 꼬리 제거
+            if board[nx][ny] == 0:
+                board[nx][ny] = 2
+                q.append((nx, ny))
+                px, py = q.pop(0)
+                board[px][py] = 0
+            # 사과가 있다면 이동 후에 꼬리 그대로 두기
+            if board[nx][ny] == 1:
+                board[nx][ny] = 2
+                q.append((nx, ny))
+
+        #벽이나 뱀의 몸통에 부딪혔다면 게임이 종료된다.
+        else:
+            time += 1
+            break
+        # 이동한 곳으로 뱀의 머리를 이동시킨다
+        x, y = nx, ny
+        time += 1
+
+        # 회전 할 시간인 경우
+        if index < L and time == change_direction[index][0]:
+            direction = turn(direction, change_direction[index][1])
+            index += 1
+    return time
+
+
+print(simulate())
+
+
