fa733d5c3e2e1fe3d6fa38014e768d1a5b9e0445
joorani <80900915+joorani@users.noreply.github.com>
2022-08-18 13:19:22 +0900

    [Silver II] Title: 에디터, Time: 516 ms, Memory: 177080 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,41 @@
+'''
+에디터
+'''
+
+a = input()
+n = int(input())
+stack = []
+tmp = [] # pop한 원소 저장할 테이블
+
+for s in a:
+    stack.append(s)
+
+for _ in range(n):
+    command = input().split()
+    com = command[0] #명령어
+    if com == 'P':
+        stack.append(command[1])
+
+    elif com == 'L':
+        if stack:
+            tmp.append(stack.pop())
+        else:
+            continue
+    elif com == 'B':
+        if stack:
+            stack.pop()
+        else:
+            continue
+    elif com == "D":
+        if tmp:
+            stack.append(tmp.pop())
+        else:
+            continue
+
+#tmp에 남은 원소 붙여주기
+if tmp:
+    stack.extend(list(reversed(tmp)))
+
+
+for x in stack:
+    print(x, end='')
