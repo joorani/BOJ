7e7bf9d5541ef1064c95405629fb3f7ea510714f
joorani <80900915+joorani@users.noreply.github.com>
2022-09-07 11:02:30 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 208 ms, Memory: 141452 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,22 +1,22 @@
-'''
-요세푸스 문제
-'''
 from collections import deque
-
+#
 n, k = map(int, input().split())
-q = deque()
-#1~ n+1까지 초기화
-for i in range(1, n+1):
-    q.append(i)
-
+q = deque([i for i in range(1, n+1)]) # 1~n까지 초기화
 answer = []
+# while q:
+#     if len(q)== 1:
+#         answer.append(q[0])
+#         break
+#     for _ in range(k-1):
+#         q.append(q.popleft())
+#     answer.append(q.popleft())
+#
+# # # 결과 출력
+# print('<' + ', '.join(map(str, answer)) + '>')
+
+# 2번째 방법 rotate(이용)
 while q:
-    if len(q)== 1:
-        answer.append(q[0])
-        break
-    for _ in range(k-1):
-        q.append(q.popleft())
+    q.rotate(-(k-1))
     answer.append(q.popleft())
 
-# # 결과 출력
 print('<' + ', '.join(map(str, answer)) + '>')
