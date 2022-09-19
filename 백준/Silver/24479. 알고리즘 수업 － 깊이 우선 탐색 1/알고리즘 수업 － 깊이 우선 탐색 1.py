5fda0da8480c46683ea6036d6f7223f82537e123
joorani <80900915+joorani@users.noreply.github.com>
2022-09-19 10:22:23 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 680 ms, Memory: 158220 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+n, m, r = map(int, input().split())
+g = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    g[a].append(b)
+    g[b].append(a) #무방향 그래프이기 때문에 양쪽에 넣어줘야함.
+
+visited = [0] * (n+1) # 방문 순서 기록, cnt를 저장할 테이블임
+cnt = 1 #방문 순서를 기록할 변수
+
+def dfs(s):
+    global cnt
+    g[s].sort()
+    for i in g[s]:
+        if visited[i] == 0:
+            cnt += 1
+            visited[i] = cnt
+            dfs(i)
+
+
+visited[r] = cnt
+dfs(r)
+for x in range(1, n+1):
+    print(visited[x])
+
+
