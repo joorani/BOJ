c49bc3298d4fc185846dd51b9a484e1ddcc5f523
joorani <80900915+joorani@users.noreply.github.com>
2022-09-28 21:10:05 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int score = Integer.parseInt(br.readLine());
+
+        String result = score >= 90 ? "A" : score >= 80 ? "B" : score >= 70 ? "C" : score >= 60 ? "D" : "F";
+        System.out.println(result);
+
+    }
+
+}
