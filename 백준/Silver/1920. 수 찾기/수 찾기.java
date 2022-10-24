e27f3881ac9b867d0b272a61f3ce0adce8b5ca9e
joorani <80900915+joorani@users.noreply.github.com>
2022-10-24 11:03:42 +0900

    [Silver IV] Title: 수 찾기, Time: 1384 ms, Memory: 52624 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,28 +1,34 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // n(데이터 개수), m(탐색할 숫자 개수)
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         int[] data = new int[n];
 
         //데이터 배열 값 입력받기
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            data[i] = sc.nextInt();
+            data[i] = Integer.parseInt(st.nextToken());
         }
         //이분탐색 이용하기 위해 데이터배열 정렬하기
         Arrays.sort(data);
 
-        int m = sc.nextInt();
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
             boolean find = false;
-            int target = sc.nextInt();
+            int target = Integer.parseInt(st.nextToken());
             //이진탐색 시작
             int startIdx = 0;
             int endIdx = n-1;
